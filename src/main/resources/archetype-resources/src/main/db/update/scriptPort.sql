1d4d061d574e258e64abd9b2234cdcdc9ce2ac03
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2012-02-26 20:31:26 +0100

    Updating of db schema

--- src/main/resources/archetype-resources/src/main/db/update/scriptPort.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO localstrings (keycode, langcode, stringvalue) VALUES ('SEARCH_BY', 'en', 'Search by');
-INSERT INTO localstrings (keycode, langcode, stringvalue) VALUES ('SEARCH_BY', 'it', 'Cerca per');
-
-ALTER TABLE showletcatalog ADD COLUMN maingroup character varying(20);
-
-ALTER TABLE resources RENAME "xml"  TO resourcexml;
